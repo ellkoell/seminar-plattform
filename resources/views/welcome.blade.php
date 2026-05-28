1d55797faee2d5399b4a2b35e7a4556508a978f0
tledere <tledere@tsn.at>
2026-05-28 11:20:05 +0200

    Routing angepasst

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,12 @@
 <body>
 <h1>Hello, World!</h1>
 
+<nav>
+    <a href="/">Home</a>
+    <a href="/about">About Us</a>
+    <a href="/contact">Contact</a>
+</nav>
+
 <div>
     <a href="/about">About us</a>
 </div>
