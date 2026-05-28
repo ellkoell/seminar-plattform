d5339f31221e6a4d4dca0bdcd2663f4af2efcdda
tledere <tledere@tsn.at>
2026-05-28 13:23:05 +0200

    welcome.blade.php: x-layout ersetzt Codeduplikat

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Document</title>
-</head>
-<body>
+<x-layout>
 <h1>Hello, World!</h1>
 
 <nav>
@@ -13,9 +6,4 @@
     <a href="/about">About Us</a>
     <a href="/contact">Contact</a>
 </nav>
-
-<div>
-    <a href="/about">About us</a>
-</div>
-</body>
-</html>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
 
-
-Route::get('/about', function () {
-    return view('about');
-});
+Route::view('/about', 'about');
+Route::view('/contact', 'contact');
