56bf9da21fd29c16058407e03a16252be5a13ec1
tledere <tledere@tsn.at>
2026-05-28 20:48:21 +0200

    Layout wieder reduziert um nur wichtigste Änderungen zu sehen

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,46 +1,27 @@
-@props([
-'title' => 'Laracasts'
-])
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>{{ $title }}</title>
+<style>
+    .max-w-400 {
+        max-width: 400px;
+        margin: auto;
+    }
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ $title }}</title>
-
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 40px;
-        }
-
-        nav {
-            margin-bottom: 20px;
-        }
-
-        nav > a {
-            color: blue;
-            margin-right: 12px;
-            text-decoration: none;
-        }
-
-        nav > a:hover {
-            text-decoration: underline;
-        }
-    </style>
+    .card {
+        background: #e3e3e3;
+        padding: 1rem;
+        text-align: center
+    }
+</style>
+<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body>
-
 <nav>
     <a href="/">Home</a>
     <a href="/about">About Us</a>
-    <a href="/contact">Contact</a>
+    <a href="/contact">Contact Us</a>
 </nav>
-
+<body class="bg-gray-700 p-6 max-w-xl m-auto">
 <main>
     {{ $slot }}
 </main>
-
-</body>
-</html>
