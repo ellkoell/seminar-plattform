8e0389e80d32abb0169e43419fbe33d152063dab
finn <fnothdurfter@tsn.at>
2026-06-01 12:06:44 +0200

    Projekt fertig Programmiert

--- resources/views/components/ideas.blade.php
+++
@@ -1,9 +0,0 @@
-<div>
-    <a {{ $attributes->merge(['class' => 'card w-96 bg-base-100 card-lg shadow-sm']) }}>
-        <div class="card-body">
-            <p>
-                {{ $slot }}
-            </p>
-        </div>
-    </a>
-</div>
