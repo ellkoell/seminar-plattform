0ca0df8994189f4884b84fa9b947ed84d970de18
Elmira Parr <eparfjonova@tsn.at>
2026-06-19 09:16:33 +0200

    Style angepasst

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,14 +3,16 @@
 ])
 
 <!doctype html>
-<html lang="en" data-theme="retro">
+<!--<html lang="en" data-theme="retro">-->
+<html lang="en" data-theme="semiflow">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title }}</title>
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
+    <!--<link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />-->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
 <x-nav />
